8dab35b3a31686ee29701045aedc378900e7cdf5
ovo72 <inho121034@gmail.com>
2026-04-14 23:24:58 +0900

    Update save_build.php

--- save_build.php
+++ save_build.php
@@ -2,35 +2,25 @@
 session_start();
 include "db.php";
 
-if (!isset($_SESSION['user'])) {
-    die("로그인이 필요합니다.");
-}
-
 $data = json_decode(file_get_contents("php://input"), true);
 
-if (!$data) {
-    die("잘못된 요청입니다.");
-}
-
-$user = $_SESSION['user'];
+$username = $_SESSION['user'];
 
 $stmt = $conn->prepare("
-INSERT INTO builds 
-(title, cpu, mb, gpu, ram, ssd, hdd, power, user)
-VALUES 
-(:title, :cpu, :mb, :gpu, :ram, :ssd, :hdd, :power, :user)
+    INSERT INTO builds (title, cpu, mb, gpu, ram, ssd, hdd, power, username)
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
 ");
 
 $stmt->execute([
-    ':title' => $data['title'],
-    ':cpu' => $data['cpu'],
-    ':mb' => $data['mb'],
-    ':gpu' => $data['gpu'],
-    ':ram' => $data['ram'],
-    ':ssd' => $data['ssd'],
-    ':hdd' => $data['hdd'],
-    ':power' => $data['power'],
-    ':user' => $user
+    $data['title'],
+    $data['cpu'],
+    $data['mb'],
+    $data['gpu'],
+    $data['ram'],
+    $data['ssd'],
+    $data['hdd'],
+    $data['power'],
+    $username
 ]);
 
 echo "저장 완료!";
